8edbd238364185be2a6a664eaa9f13d328a5dd6b
Ashutosh Pradhan <ashutosh.pradhan0049@gmail.com>
2025-09-07 19:52:00 +0530

    auth provider setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
 import { BrowserRouter } from "react-router"
+import { Toaster } from "react-hot-toast"
+
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import AuthProvider from './providers/AuthProvider.jsx'
+
+const queryClient = new QueryClient();
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
@@ -15,7 +24,12 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <BrowserRouter>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+          <Toaster position='top-right'/>
+        </QueryClientProvider>
       </BrowserRouter>
       </ClerkProvider>
   </StrictMode>,
